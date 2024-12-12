2fa3303176819156dbbeeee8e058e013bb5e2717
unknown <daniele.lemessantos@outlook.com>
2024-12-11 23:57:40 -0300

    Commit inicial

---
+++ src/main/java/br/com/goldfood/GoldFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.goldfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goldfood/GoldFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goldfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
