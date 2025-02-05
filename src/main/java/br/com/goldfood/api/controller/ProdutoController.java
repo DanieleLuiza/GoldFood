dcc5f80dd355897f7de9a3b57a0a176f037caa89
unknown <daniele.lemessantos@outlook.com>
2025-02-04 22:45:16 -0300

    Criado Endpoint listar-produto

--- src/main/java/br/com/goldfood/api/controller/ProdutoController.java
+++ src/main/java/br/com/goldfood/api/controller/ProdutoController.java
@@ -1,14 +1,18 @@
 package br.com.goldfood.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.goldfood.api.dto.ProdutoDTORequest;
+import br.com.goldfood.core.dto.entity.ProdutoEntity;
 import br.com.goldfood.core.facade.ProdutoFacade;
 
 @RestController
@@ -22,6 +26,12 @@ public class ProdutoController {
 	public ResponseEntity<String> cadastroProduto(@RequestBody ProdutoDTORequest request){
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(facade.cadastrar(request));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ProdutoEntity>> listarProduto(){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(facade.listar());
 	}
 
 }
--- src/main/java/br/com/goldfood/core/facade/ProdutoFacade.java
+++ src/main/java/br/com/goldfood/core/facade/ProdutoFacade.java
@@ -1,9 +1,12 @@
 package br.com.goldfood.core.facade;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.goldfood.api.dto.ProdutoDTORequest;
+import br.com.goldfood.core.dto.entity.ProdutoEntity;
 import br.com.goldfood.core.service.ProdutoService;
 
 @Component
@@ -15,5 +18,11 @@ public class ProdutoFacade {
 	public String cadastrar(ProdutoDTORequest request) {
 		
 		return service.cadastrar(request);
+	}
+	
+
+	public List<ProdutoEntity> listar() {
+		
+		return service.listar();
 	}
 }
