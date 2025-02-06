93ce2a663b15c15b3b3f7894053500ccf81f960a
unknown <daniele.lemessantos@outlook.com>
2025-02-05 21:18:22 -0300

    Criado Endpoint listar-fornecedor

--- src/main/java/br/com/goldfood/api/controller/FornecedorController.java
+++ src/main/java/br/com/goldfood/api/controller/FornecedorController.java
@@ -1,5 +1,7 @@
 package br.com.goldfood.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.goldfood.api.dto.FornecedorDTORequest;
+import br.com.goldfood.core.dto.entity.FornecedorEntity;
 import br.com.goldfood.core.facade.FornecedorFacade;
 
 @RestController
@@ -22,6 +25,11 @@ public class FornecedorController {
 	public ResponseEntity<String> cadastroFornecedor(@RequestBody FornecedorDTORequest request){
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(facade.cadastrar(request));
+	}
+	
+	public ResponseEntity<List<FornecedorEntity>> listarFornecedor(){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(facade.listar());
 	}
 
 }
--- src/main/java/br/com/goldfood/core/facade/FornecedorFacade.java
+++ src/main/java/br/com/goldfood/core/facade/FornecedorFacade.java
@@ -1,9 +1,12 @@
 package br.com.goldfood.core.facade;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.goldfood.api.dto.FornecedorDTORequest;
+import br.com.goldfood.core.dto.entity.FornecedorEntity;
 import br.com.goldfood.core.service.FornecedorService;
 
 @Component
@@ -15,6 +18,11 @@ public class FornecedorFacade {
 	public String cadastrar(FornecedorDTORequest request) {
 		
 		return service.cadastrar(request);
+	}
+
+	public List<FornecedorEntity> listar() {
+		
+		return service.listar();
 	}
 
 }
